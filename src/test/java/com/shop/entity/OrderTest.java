795aece3bdf51f0234d13f0ba53b81ea8fa2c911
gurrms95 <gurrms95@gmail.com>
2023-06-28 17:43:49 +0900

    Auditing~

--- src/test/java/com/shop/entity/OrderTest.java
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -2,6 +2,8 @@ package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
 import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderItemRepository;
 import com.shop.repository.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,7 +27,13 @@ public class OrderTest {
     OrderRepository orderRepository;
 
     @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
 
     @PersistenceContext
     EntityManager em;
@@ -65,4 +73,46 @@ public class OrderTest {
         assertEquals(3,saveOrder.getOrderItems().size());
     }
 
+    public Order createOrder(){
+        Order order = new Order();
+        for(int i=0 ; i<3 ; i++){
+            Item item = createTime();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(1000);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+        Member member = new Member();
+        memberRepository.save(member);
+
+        order.setMember(member);
+        orderRepository.save(order);
+        return order;
+    }
+
+    @Test
+    @DisplayName("고아객체 제거 테스트")
+    public void orphanRemovalTest(){
+        Order order = this.createOrder();
+        order.getOrderItems().remove(0);
+        em.flush();
+    }
+
+    @Test
+    @DisplayName("지연 로딩테스트")
+    public void lazyLoadingTest(){
+        Order order = this.createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+
+        OrderItem orderItem = orderItemRepository.findById(orderItemId).orElseThrow(EntityNotFoundException::new);
+        System.out.println("Order class:"+orderItem.getOrder().getClass());
+        System.out.println("=============================================");
+        orderItem.getOrder().getOrderDate();
+        System.out.println("=============================================");
+    }
 }
